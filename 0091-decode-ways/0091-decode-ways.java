ca65c3193591cde5c1bf35881a6cb3d2492d6616
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2023-10-26 15:45:03 +0530

    Time: 6 ms (8.34%), Space: 41.3 MB (9.98%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,41 +1,25 @@
 class Solution {
 
-    public int solve(String s, int i, int[] dp) {
-        if(i == s.length()) {
-            dp[i] = 1;
-            return 1;
-        } else if(i > s.length()) {
-            if(i==s.length()) {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+
+        dp[n] = 1;
+        for(int i=n-1; i>=0; i--) {
+            if(s.charAt(i)!='0') {
+                dp[i] = dp[i+1];
+                if(i+1 < n) {
+                    int num = Integer.parseInt(s.charAt(i) + ""+ s.charAt(i+1));
+                    if(num <= 26) {
+                        dp[i] += dp[i+2];
+                    }
+                }
+            } else {
                 dp[i] = 0;
             }
-            return 0;
         }
-        if(dp[i]!= -1) {
-            return dp[i];
-        }
+        
+        return dp[0];
 
-        if(s.charAt(i) == '0') {
-            dp[i] = 0;
-            return 0;
-        }
-            
-        int a = solve(s, i+1, dp);
-        int b=0;
-        if(i+1 < s.length()) {
-            int num = Integer.parseInt(s.charAt(i) + ""+ s.charAt(i+1));
-            if(num <= 26) {
-                b = solve(s, i+2, dp);
-            }
-        }
-        dp[i] = a+b;
-        return dp[i];
-    }
-
-    public int numDecodings(String s) {
-        int[] dp = new int[s.length()+1];
-        for(int i=0; i<s.length()+1; i++) {
-            dp[i] = -1;
-        }
-        return solve(s, 0, dp);
     }
 }
