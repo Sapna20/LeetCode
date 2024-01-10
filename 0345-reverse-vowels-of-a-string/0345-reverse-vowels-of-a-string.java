e80b918bacadc38a3c73be40c76a4b19ae74c8e7
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2024-01-10 22:05:17 +0530

    Time: 8 ms (28.21%), Space: 44.9 MB (26.49%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,28 @@
 class Solution {
-    public String reverseVowels(String s) {
-        int i = 0;
-        int j = s.length()-1;
-        int n = j+1;
-        List<Character> ls = new ArrayList<Character> ();
-        ls.addAll(Arrays.asList('a','e','i','u','o','A','E','I','U','O'));
-        char[] arr = s.toCharArray();
-        while(i<n && j>= 0 && i<j) {
-            while(i<n && !ls.contains(s.charAt(i))) {
+    public String reverseVowels(String str) {
+        int i=0, j=str.length()-1;
+        List<Character> ls = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+
+        char[] s = str.toCharArray();
+
+        while(i<j) {
+            while(i<j && !ls.contains(s[i])) {
                 i++;
             }
-            while(j>=0 && !ls.contains(s.charAt(j))) {
+            while(i<j && !ls.contains(s[j])) {
                 j--;
             }
-            if(i<n && j>= 0 && i<j) {
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+
+            if(ls.contains(s[i]) && ls.contains(s[j])) {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
             }
             i++;
             j--;
         }
-        return String.valueOf(arr);
+
+        return String.valueOf(s);
+
     }
 }
