545620d1b8009bae0bc737a1e1daa7f81ca1d159
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2024-11-01 16:53:22 +0530

    Time: 2 ms (97.84%), Space: 44.5 MB (46.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,6 +3,7 @@ class Solution {
         if(s.length() != t.length()) {
             return false;
         }
+        int n = s.length();
 
         char[] s_arr = s.toCharArray();
         char[] t_arr = t.toCharArray();
@@ -10,6 +11,13 @@ class Solution {
         Arrays.sort(s_arr);
         Arrays.sort(t_arr);
 
-        return Arrays.equals(s_arr, t_arr);
+        String s_temp = String.valueOf(s_arr);
+        String t_temp = String.valueOf(t_arr);
+
+        if(s_temp.equals(t_temp)) {
+            return true;
+        }
+
+        return false;
     }
 }
