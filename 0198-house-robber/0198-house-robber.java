5f0f57ff823920fdad1fcc3c21cf906fe3a6f62d
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-01-01 16:12:27 +0530

    Time: 0 ms (100%), Space: 40.8 MB (86.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,27 @@
 class Solution {
 
+    // Recursive
+    // private int solve(int[] nums, int index) {
+    //     if(index < 0) {
+    //         return 0;
+    //     }
+
+    //     return Math.max( nums[index] + solve(nums, index-2), solve(nums, index-1));
+    // }
+
     public int rob(int[] nums) {
+        // return solve(nums, nums.length-1);
+
+        // iterative + memo
         int n = nums.length;
-        if(n == 1) {
-            return nums[0];
-        }
-        int[] dp = new int[n+2]; 
-        for(int i=n-1; i>=0; i--) {
-            dp[i] = Math.max(nums[i]+dp[i+2], dp[i+1]);
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<n+1; i++) {
+            dp[i] = Math.max( nums[i-1] + dp[i-2], dp[i-1]);
         }
 
-        return dp[0];
+        return dp[n];
     }
 }
