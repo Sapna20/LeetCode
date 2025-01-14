65aa3418af13c455e864a44a32646e63c22ae4ad
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-01-14 14:24:07 +0530

    Time: 1 ms (81.41%), Space: 41.8 MB (69.47%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,25 +1,43 @@
 class Solution {
 
+    private int solveRecursive(String s, int index, int[] dp) {
+        if (index < 0) {
+            return 1;
+        }
+
+        if(dp[index] != -1) 
+            return dp[index];
+
+        int a=0, b=0;
+        int num; 
+
+        if(index - 1 >= 0) {
+            num = Integer.parseInt(s.substring(index-1, index+1));
+        } else {
+            num = Integer.parseInt(s.substring(index, index+1));
+        }
+
+        if(num % 10 > 0) {
+            a = solveRecursive(s, index-1, dp);
+        }
+        if(num >= 10 && num <= 26)
+        {
+            b = solveRecursive(s, index-2, dp);
+        }
+
+        dp[index] = a+b;
+        
+        return dp[index];
+    }
+
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n+1];
 
-        dp[n] = 1;
-        for(int i=n-1; i>=0; i--) {
-            if(s.charAt(i)!='0') {
-                dp[i] = dp[i+1];
-                if(i+1 < n) {
-                    int num = Integer.parseInt(s.charAt(i) + ""+ s.charAt(i+1));
-                    if(num <= 26) {
-                        dp[i] += dp[i+2];
-                    }
-                }
-            } else {
-                dp[i] = 0;
-            }
-        }
-        
-        return dp[0];
+        int[] dp = new int[n]; 
 
+        for(int i=0; i<n; i++) 
+            dp[i] = -1;
+
+        return solveRecursive(s, n-1, dp);
     }
 }
