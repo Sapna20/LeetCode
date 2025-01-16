a44e61b16637c9655e93444108d8216bf8a9998b
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-01-16 23:35:56 +0530

    Time: 15 ms (78.03%), Space: 43.7 MB (99.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,30 @@
 class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int m = coins.length + 1;
+        int n = amount + 1;
+        int[][] dp = new int[m][n];
 
-    private int solveRecursive(int[] coins, int amount, int index, int[][] dp) {
-        if(amount == 0) 
-            return 0;
-            
-        if(index < 0) 
-            return Integer.MAX_VALUE-12;
-        
-        if(dp[index][amount] != -1)
-            return dp[index][amount];
-
-        if(amount - coins[index] >= 0) {
-            dp[index][amount] = Math.min(
-                1+solveRecursive(coins, amount - coins[index], index, dp),
-                solveRecursive(coins, amount, index - 1, dp)
-            );
-        } else {
-            dp[index][amount] = solveRecursive(coins, amount, index-1, dp);
+        for(int i=0; i<m; i++) {
+            dp[i][0] = 0;
+        }
+        for(int j=1; j<n; j++) {
+            dp[0][j] = Integer.MAX_VALUE-1;
         }
 
-        return dp[index][amount];
-    }
-
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<=amount; j++) {
-                dp[i][j] = -1;
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if(j-coins[i-1] >= 0) {
+                    dp[i][j] = Math.min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        int ans = solveRecursive(coins, amount, n-1, dp);
 
-        return ans >= Integer.MAX_VALUE-12 ? -1 : ans;
+        if(dp[m-1][n-1] >= Integer.MAX_VALUE-1 ) {
+            return -1;
+        }
+
+        return dp[m-1][n-1];
     }
 }
