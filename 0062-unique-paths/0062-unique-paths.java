7f7a827ca58c5b96c19e425f5e33949a7c9f521d
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-02-01 22:04:19 +0530

    Time: 0 ms (100%), Space: 40.3 MB (87.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,27 +1,29 @@
 class Solution {
 
+    private int recurSolve(int m, int n, int i, int j, int[][] dp) {
+        if(i==m-1 && j==n-1) {
+            return 1;
+        } else if(i>=m || j>=n || j<0 || i<0) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        dp[i][j] = recurSolve(m, n, i+1, j, dp) + recurSolve(m, n, i, j+1, dp);
+
+        return dp[i][j];
+    }
+
+
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-
-        for(int i=0; i<m+1; i++) {
-            dp[i][0] = 0;
-            if(i>=1) {
-                dp[i][1] = 1;
-            }
-        }
-        for(int j=0; j<n+1; j++) {
-            dp[0][j] = 0;
-            if(j>=1) {
-                dp[1][j] = 1;
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                dp[i][j] = -1;
             }
         }
 
-        for(int i=2; i<m+1; i++) {
-            for(int j=2; j<n+1; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-
-        return dp[m][n];
+        return recurSolve(m, n, 0, 0, dp);
     }
 }
