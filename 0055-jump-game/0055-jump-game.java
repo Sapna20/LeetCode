bc809dc3a3ebbe25415e77d5b3a27cd1379deb9d
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-03-25 11:04:28 +0530

    Time: 4 ms (16%), Space: 43.4 MB (99.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,38 @@
 class Solution {
 
-    private boolean canReachLast(int[] nums, int index) {
-        if(index == nums.length-1)
-            return true;
-        
-        boolean ans = false;
-
-        for(int i=1; i<=nums[index]; i++) {
-            ans = ans || canReachLast(nums, index+i);
+    private boolean canSubJump(int[] nums, int dind, int[] dp) {
+        boolean result = false;
+        for(int i=dind-1; i>=0; i--) {
+            if(dp[i] == 0) {
+                return false;
+            }
+            if(dp[i] == 1) {
+                return true;
+            }
+            if(dind-i <= nums[i]) {
+                if(i==0) {
+                    return true;
+                }
+                else {
+                    result = result || canSubJump(nums, i, dp);
+                    dp[i] = result == false ? 0 : 1;
+                } 
+            } 
         }
 
-        return ans;
+        return result;
     }
 
     public boolean canJump(int[] nums) {
-        return canReachLast(nums, 0);
+        if(nums.length == 1) {
+            return true;
+        }
+        int[] dp = new int[nums.length];
+
+        for(int i=0; i<nums.length; i++) {
+            dp[i] = -1;
+        }
+
+        return canSubJump(nums, nums.length-1, dp);
     }
 }
