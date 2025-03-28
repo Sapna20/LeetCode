f4cfcecaca38c2bc25cc04a41ee63b89bd23ba09
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-03-28 23:24:51 +0530

    Time: 8 ms (86.92%), Space: 46.3 MB (96.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,31 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        Arrays.sort(intervals, (i, j) -> {return i[0]-j[0];});
-
-        List<List<Integer>> ansLs = new ArrayList<List<Integer>> ();
-
-        for(int i=0; i<n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            while(i+1 < n && end >= intervals[i+1][0]) {
-                end = Math.max(intervals[i+1][1], end);
+        int[][] mergedIntervals = new int[n][2];
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        int i=0;
+        int j=0;
+        while(i < n) {
+            int[] temp = new int[2];
+            temp[0] = intervals[i][0];
+            temp[1] = intervals[i][1];
+            i++;
+            while(i < n && temp[1] >= intervals[i][0]) {
+                temp[1] = Math.max(temp[1], intervals[i][1]);
                 i++;
             }
-            List<Integer> ls = new ArrayList<Integer> ();
-            ls.add(start);
-            ls.add(end);
-            ansLs.add(ls);
-        }
 
-        int[][] ans = new int[ansLs.size()][2];
-        int j=0;
-        for(List<Integer> list : ansLs) {
-            ans[j][0] = list.get(0);
-            ans[j][1] = list.get(1);
+            mergedIntervals[j] = temp;
             j++;
         }
-        return ans;
+
+        return Arrays.copyOfRange(mergedIntervals, 0, j);
     }
 }
