83c7a7fc295c1dce70e4465e4196fb14a357f814
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2025-06-06 00:28:43 +0530

    Time: 7 ms (65.17%), Space: 47.2 MB (99.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,24 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        List<List<String>> ans = new ArrayList<List<String>>();
 
         for(String str : strs) {
-            char[] tempArr = str.toCharArray();
-            Arrays.sort(tempArr);
-            String tempStr = String.valueOf(tempArr);
-            if(map.containsKey(tempStr)) {
-                map.get(tempStr).add(str);
-            } else {
-                List<String> ls = new ArrayList<String>();
-                ls.add(str);
-                map.put(tempStr, ls);
-            }
+            char[] cstr = str.toCharArray();
+            Arrays.sort(cstr);
+            String key = String.valueOf(cstr);
+            List<String> ls = map.getOrDefault(key, new ArrayList<String>());
+            ls.add(str);
+            map.put(key, ls);
         }
 
-        List<List<String>> ans = new ArrayList<List<String>> ();
-
-        for(List<String> value : map.values()) {
-            ans.add(value);
+        for(List<String> anagrams : map.values()) {
+            ans.add(anagrams);
         }
 
         return ans;
