e2a23214fb7296e717e90c9bd1fd36cfbfc150b0
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-04-20 12:47:15 +0530

    Time: 5 ms (61.73%), Space: 44.1 MB (95.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,14 +3,19 @@ class Solution {
         if(s.length() != t.length())
             return false;
 
-        Map<Character, Integer> mapS = new HashMap<>();
-        Map<Character, Integer> mapT = new HashMap<>();
+        int[] arr = new int[26];
 
         for(int i=0; i<s.length(); i++) {
-            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i), 0) + 1);
-            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        return mapS.equals(mapT);
+        for(int i=0; i<26; i++) {
+            if(arr[i]!=0)
+                return false;
+        }
+
+        return true;
+
     }
 }
