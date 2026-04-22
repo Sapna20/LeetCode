0948289ae16d13bca1b1dfb5638c352bafc7852c
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-04-22 17:25:43 +0530

    Time: 4 ms (40.98%), Space: 43.2 MB (59.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,33 +1,37 @@
 class Solution {
 
-    public boolean isValidPair(char open, char close) {
-        if((open == '{' && close != '}')
-            || (open == '(' && close != ')')
-            || (open == '[' && close != ']') ) {
-                return false;
-            }
-        return true;
+    private boolean isOpen(Character c) {
+        return c == '[' || c == '(' || c == '{';
+    }
+
+    private boolean isClose(Character c) {
+        return c == ']' || c == ')' || c == '}';
+    }
+
+    private boolean isValidPair(Character open, Character close) {
+        return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
+        
     }
 
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-        List<Character> openBrackets = new ArrayList<>(Arrays.asList('{', '(', '['));
-        List<Character> closeBrackets = new ArrayList<>(Arrays.asList('}', ')', ']'));
+        Stack<Character> st = new Stack<>();
 
         for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(openBrackets.contains(c)) {
+            Character c = s.charAt(i);
+            if(isOpen(c)) {
                 st.push(c);
-            } else if(closeBrackets.contains(c) && !st.isEmpty()) {
-                char tc = st.peek();
-                if(!isValidPair(tc, c))
+                continue;
+            } 
+
+            if(!st.isEmpty()) {
+                Character z = st.pop();
+                if(!isValidPair(z, c))
                     return false;
-                st.pop();
             } else {
                 return false;
             }
+        
         }
-
         return st.isEmpty();
     }
 }
