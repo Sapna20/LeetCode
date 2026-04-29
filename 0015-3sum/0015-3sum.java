e7015fad61504db3810d479bbe65022666b4d3f7
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-04-29 09:49:19 +0530

    Time: 41 ms (19.74%), Space: 59.1 MB (60.04%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
 
         for(int i=0; i<n; i++) {
-            if(i > 0) {
-                while( i<n && nums[i-1] == nums[i]) {
-                    i++;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int target = -nums[i];
+            int j=i+1, k=n-1;
+            while(j<k) {
+                if(j>i+1 && nums[j]==nums[j-1]) {
+                    j++; continue;
+                } 
+                if(nums[j] + nums[k] == target) {
+                    ans.add(new ArrayList<Integer>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    j++;
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else {
+                    j++;
                 }
-                if(i >= n)
-                    break;
             }
-                int x = nums[i];
-                int L = i+1;
-                int R = n-1;
-
-                while( L < R ) {
-                    int y = nums[L];
-                    int z = nums[R];
-                    int sum = x + y + z;
-                    if(sum == 0) {
-                        List<Integer> ls = new ArrayList<Integer>();
-                        ls.add(x);
-                        ls.add(y);
-                        ls.add(z);
-                        ans.add(ls);
-                        L++;
-                        while(L<n && nums[L-1] == nums[L]){
-                            L++;
-                        }
-                    } else if(sum > 0) {
-                        R--;
-                    } else {
-                        L++;
-                    }
-                }
         }
 
         return ans;
