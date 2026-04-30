1d36a06605e964702bc92e166de1e9b635f9638b
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-04-30 23:50:32 +0530

    Time: 0 ms (100%), Space: 46.9 MB (33.07%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slowPtr = head;
         ListNode fastPtr = head;
+        ListNode slowPtr = head;
 
-        while(slowPtr != null && fastPtr != null && fastPtr.next != null) {
+        while(fastPtr != null && fastPtr.next != null) {
             slowPtr = slowPtr.next;
             fastPtr = fastPtr.next.next;
-
             if(slowPtr == fastPtr) {
                 return true;
-            }   
+            }
         }
 
         return false;
