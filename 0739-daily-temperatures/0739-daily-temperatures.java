54f02925ce8b597b6471f6a39bf772e8df28e87c
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-04-30 16:34:31 +0530

    Time: 76 ms (27.91%), Space: 97.3 MB (99.05%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,33 @@
+class Pair {
+    int data;
+    int idx;
+
+    Pair(int data, int idx) {
+        this.data = data;
+        this.idx = idx;
+    }
+}
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Pair> st = new Stack<Pair>();
         int n = temperatures.length;
-        Stack<Integer> st = new Stack<Integer>();
         int[] ans = new int[n];
 
         for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && temperatures[st.peek()] <= temperatures[i]) {
+            while(!st.isEmpty() && st.peek().data <= temperatures[i]) {
                 st.pop();
             }
-
-            if(st.empty()) {
+            
+            if(st.isEmpty()) {
                 ans[i] = 0;
             } else {
-                ans[i] = st.peek() - i;
-            } 
-            st.push(i);
-        }     
+                ans[i] = st.peek().idx - i;
+            }
 
-        return ans;  
+            st.push(new Pair(temperatures[i], i));
+        }
+
+        return ans;
     }
 }
