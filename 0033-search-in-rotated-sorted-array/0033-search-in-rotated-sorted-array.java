eba7abcfeac933280aae125c777b98852ff27108
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-05-11 12:26:55 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,37 +1,35 @@
 class Solution {
 
-    private int getPivotIndex(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
+    private int searchStartIndex(int[] nums) {
         int n = nums.length;
+        int start = 0;
+        int end = n-1;
 
         while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(nums[mid] <= nums[(mid+1) % n] && nums[mid] <= nums[(mid-1+n) % n]) {
+            int mid = (end-start)/2 + start;
+            if(nums[mid] <= nums[(mid-1+n) % n]) {
                 return mid;
-            } else if(nums[mid] > nums[n-1]) {
+            } else if (nums[mid] < nums[start]) {
+                end = mid-1;
+            } else if (nums[mid] > nums[end]) {
                 start = mid+1;
             } else {
                 end = mid-1;
             }
         }
 
-        return -1;
+        return Integer.MAX_VALUE;
     }
 
-    private int binarySearch(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-
+    private int binarySearch(int[] nums, int target, int start, int end) {
         while(start <= end) {
-            int mid = start + (end - start)/2;
-
-            if(nums[mid] == target) {
+            int mid = (end-start)/2 + start;
+            if(target == nums[mid]) {
                 return mid;
-            } else if(nums[mid] > target) {
-                end = mid-1;
-            } else {
+            } else if (target > nums[mid]) {
                 start = mid+1;
+            } else {
+                end = mid-1;
             }
         }
 
@@ -39,16 +37,12 @@ class Solution {
     }
 
     public int search(int[] nums, int target) {
-        int p = getPivotIndex(nums);
-
-        if(p == -1) {
-            return -1;
-        }
-
-        int optionA = binarySearch(Arrays.copyOfRange(nums, 0, p), target);
-        int optionB = binarySearch(Arrays.copyOfRange(nums, p, nums.length), target);
-
-        return optionA != -1 ? optionA : optionB != -1 ? optionB + p : -1;
-
+        int startIndex = searchStartIndex(nums);
+        int n = nums.length;
+        
+        return target >= nums[startIndex] && target <= nums[n-1] 
+                            ? binarySearch(nums, target, startIndex, nums.length-1)
+                            : binarySearch(nums, target, 0, startIndex-1);
+        
     }
 }
