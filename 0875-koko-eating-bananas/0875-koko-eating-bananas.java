abdb6d28469b263ee4d8bd333ce67f6f500dbc6c
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-05-15 10:50:31 +0530

    Time: 25 ms (23.86%), Space: 47.6 MB (85.4%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,30 +1,38 @@
 class Solution {
 
-    private int getTotalHoursByRate(int[] piles, int r) {
-        int hrs = 0;
-        for(int i=0; i<piles.length; i++) {
-            hrs += (int)Math.ceil((double)piles[i]/r);
+    private boolean isValid(int mid, int[] piles, int h) {
+        int n = piles.length;
+        int total_hours = 0;
+
+        for(int i=0; i<n; i++) {
+            total_hours += Math.ceil((double)piles[i]/(double)mid);
         }
-        return hrs;
+
+        return total_hours <= h;
     }
 
     public int minEatingSpeed(int[] piles, int h) {
-        int rates = Arrays.stream(piles).max().getAsInt();
-        int start = 1, end = rates;
-        int ans = rates;
+        int k = 0;
+        int start = 1;
+        int end = 0;
+
+        for(int x : piles) {
+            end = Math.max(end, x);
+        }
+        // System.out.println("end = " + end);
 
         while(start <= end) {
-            int mid = start + (end-start)/2;
-            int tempH = getTotalHoursByRate(piles, mid);
-
-            if(h >= tempH) {
-                ans = Math.min(mid, ans);
+            int mid = (end-start)/2 + start;
+            // System.out.println("mid = " + mid);
+            if(isValid(mid, piles, h)) {
+                k = mid;
                 end = mid-1;
-            } else if(h < tempH) {
+                // System.out.println("k = " + k);
+            } else {
                 start = mid+1;
             }
         }
-
-        return ans;
+        // System.out.println("piles[0] " + piles[0]);
+        return k;
     }
 }
