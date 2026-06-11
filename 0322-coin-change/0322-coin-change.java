92c07f2be3f7d9a01de783e131abcf8d159739e2
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-06-11 18:28:12 +0530

    Time: 40 ms (7.7%), Space: 48.9 MB (5.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,66 +1,36 @@
 class Solution {
 
-    private int solveRecursive(int[] coins, int amount, int index, int[][] dp) {
-        if(amount == 0) 
+    private int solve(int[] coins, int amount, int idx, Integer[][] dp) {
+        if(amount <= 0) {
             return 0;
-            
-        if(index < 0) 
-            return Integer.MAX_VALUE-12;
-        
-        if(dp[index][amount] != -1)
-            return dp[index][amount];
-
-        if(amount - coins[index] >= 0) {
-            dp[index][amount] = Math.min(
-                1+solveRecursive(coins, amount - coins[index], index, dp),
-                solveRecursive(coins, amount, index - 1, dp)
-            );
-        } else {
-            dp[index][amount] = solveRecursive(coins, amount, index-1, dp);
+        }
+        if(idx < 0) {
+            return 10001;
         }
 
-        return dp[index][amount];
-    }
-
-    private int solveBottomUp(int[] coins, int amount, int n, int[][] dp) {
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = 0;
+        if(dp[idx][amount] != null) {
+            return dp[idx][amount];
         }
 
-        for(int j=1; j<=amount; j++) {
-            dp[0][j] = Integer.MAX_VALUE-1;
+        if(amount - coins[idx] >= 0) {
+            dp[idx][amount] = Math.min(
+                1 + solve(coins, amount - coins[idx], idx, dp),
+                solve(coins, amount, idx-1, dp)
+                );
+            return dp[idx][amount];
         }
 
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=amount; j++) {
-                if(j-coins[i-1] >= 0) {
-                    dp[i][j] = Math.min(
-                        1 + dp[i][j-coins[i-1]],
-                        dp[i-1][j]
-                    );
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-
-        return dp[n][amount];
+        dp[idx][amount] = solve(coins, amount, idx-1, dp);
+        return dp[idx][amount];
     }
 
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<=amount; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        
-        // int ans = solveRecursive(coins, amount, n-1, dp);
+        Integer[][] dp = new Integer[n][amount+1];
 
-        int[][] dp2 = new int[n+1][amount+1];
-        int ans = solveBottomUp(coins, amount, n, dp2);
+        int count = solve(coins, amount, coins.length-1, dp);
 
-        return ans >= Integer.MAX_VALUE-1 ? -1 : ans;
+        System.out.println(Integer.MAX_VALUE);
+        return count > 10000 ? -1 : count;
     }
 }
