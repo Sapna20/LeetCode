2060a9aefcdff1bda8800aec85f72074f4e67f21
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-06-12 17:05:24 +0530

    Time: 4 ms (85.48%), Space: 45.1 MB (42.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,16 +1,22 @@
 class Solution {
 
-    private int solve(int[] nums, int S, int idx) {
+    private int solve(int[] nums, int S, int idx, Integer[][] dp) {
         
         if(idx == nums.length) {
             return S == 0 ? 1 : 0;
         }
 
-        if(S - nums[idx] >= 0) {
-            return solve(nums, S-nums[idx], idx+1) + solve(nums, S, idx+1);
+        if(dp[idx][S] != null) {
+            return dp[idx][S];
         }
 
-        return solve(nums, S, idx+1);
+        if(S - nums[idx] >= 0) {
+            dp[idx][S] = solve(nums, S-nums[idx], idx+1, dp) + solve(nums, S, idx+1, dp);
+        } else {
+            dp[idx][S] = solve(nums, S, idx+1, dp);
+        }
+
+        return dp[idx][S];
     }
 
     public int findTargetSumWays(int[] nums, int target) {
@@ -24,7 +30,12 @@ class Solution {
         }
 
         int S = (sum + target)/2;
+        if(S < 0) {
+            return 0;
+        }
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][S+1];
 
-        return solve(nums, S, 0);
+        return solve(nums, S, 0, dp);
     }
 }
