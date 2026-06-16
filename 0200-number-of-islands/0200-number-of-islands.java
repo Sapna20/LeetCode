e95c6e56bb03914fafbe2d96e2e2f768fe0d831e
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-06-16 12:09:56 +0530

    Time: 5 ms (29.02%), Space: 52.3 MB (59.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,32 @@
 class Solution {
-    private boolean isValidLand(char[][] grid, int i, int j, boolean[][] isVisited) {
-        return i < grid.length && i >= 0 && j < grid[0].length && j>=0 && grid[i][j] == '1' && !isVisited[i][j];
+
+    private boolean isValid(char[][] grid, int i, int j, int m, int n, boolean[][] visited) {
+        return i >=0 && j>=0 && i <m && j<n && !visited[i][j] && grid[i][j] == '1';
     }
 
-    private void markVisitedLands(char[][] grid, boolean[][] isVisited, int i, int j) {
-        if(!isValidLand(grid, i, j, isVisited))
+    private void traverseIsland(char[][] grid, int i, int j, int m, int n, boolean[][] visited) {
+        if(!isValid(grid, i, j, m, n, visited)) 
             return;
         
-        isVisited[i][j] = true;
-
-        markVisitedLands(grid, isVisited, i+1, j);
-        markVisitedLands(grid, isVisited, i, j+1);
-        markVisitedLands(grid, isVisited, i-1, j);
-        markVisitedLands(grid, isVisited, i, j-1);
+        visited[i][j] = true;
+        traverseIsland(grid, i+1, j, m, n, visited);
+        traverseIsland(grid, i, j+1, m, n, visited); 
+        traverseIsland(grid, i-1, j, m, n, visited);
+        traverseIsland(grid, i, j-1, m, n, visited);
+             
+        return;
     }
 
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] isVisited = new boolean[m][n]; 
-        int count = 0;
 
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(!isVisited[i][j] && grid[i][j] == '1') {
-                    markVisitedLands(grid, isVisited, i, j);
+                if(!visited[i][j] && grid[i][j] == '1') {
+                   traverseIsland(grid, i, j, m, n, visited);
                     count++;
                 }
             }
