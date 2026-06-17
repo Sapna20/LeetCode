c98e4856b9120171370ab939ddfc13e54df61d76
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-06-17 11:49:25 +0530

    Time: 3 ms (27.19%), Space: 46.7 MB (23.83%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,34 +1,34 @@
 class Solution {
-    private boolean isValidLand(int[][] grid, int i, int j, boolean[][] isVisited) {
-        return i < grid.length && i >= 0 && j < grid[0].length && j>=0 && grid[i][j] == 1 && !isVisited[i][j];
+    private boolean isValid(int[][] grid, int i, int j, int m, int n, boolean[][] visited) {
+        return i >=0 && j>=0 && i <m && j<n && !visited[i][j] && grid[i][j] == 1;
     }
 
-    private int getIslandArea(int[][] grid, boolean[][] isVisited, int i, int j) {
-        if(!isValidLand(grid, i, j, isVisited))
+    private int traverseIsland(int[][] grid, int i, int j, int m, int n, boolean[][] visited) {
+        if(!isValid(grid, i, j, m, n, visited)) 
             return 0;
         
-        isVisited[i][j] = true;
-
-        return 1 + getIslandArea(grid, isVisited, i+1, j) +
-                getIslandArea(grid, isVisited, i, j+1) +
-                getIslandArea(grid, isVisited, i-1, j) +
-                getIslandArea(grid, isVisited, i, j-1);
+        visited[i][j] = true;
+        return 1 + traverseIsland(grid, i+1, j, m, n, visited)
+                    + traverseIsland(grid, i, j+1, m, n, visited)
+                    + traverseIsland(grid, i-1, j, m, n, visited)
+                    + traverseIsland(grid, i, j-1, m, n, visited);
     }
 
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] isVisited = new boolean[m][n]; 
-        int curr_max = 0;
 
+        boolean[][] visited = new boolean[m][n];
+        int maxArea = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(!isVisited[i][j] && grid[i][j] == 1) {
-                    curr_max = Math.max(curr_max, getIslandArea(grid, isVisited, i, j));
+                if(!visited[i][j] && grid[i][j] == 1) {
+                   maxArea = Math.max(maxArea, traverseIsland(grid, i, j, m, n, visited));
                 }
             }
         }
 
-        return curr_max;     
+        return maxArea;
     }
+    
 }
