39627f587c20cd8f77d9f1fc61810a4c23086897
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-06-27 13:42:31 +0530

    Time: 20 ms (6.07%), Space: 47.1 MB (76.23%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    private boolean solve(int[][] graph, int node, boolean[] visited, boolean[] flag) {
+        visited[node] = true;
+        System.out.println(node + " " + visited[node] + " " + flag[node]);
+        boolean ans = true;
+        for(int i=0; i<graph[node].length; i++) {
+            int adj = graph[node][i];
+            if(!visited[adj]) {
+                flag[adj] = !flag[node];
+                ans = ans & solve(graph, adj, visited, flag);
+                if(!ans) {
+                    return false;
+                }
+            } else if (flag[adj] == flag[node]) {
+                return false;
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        boolean[] visited = new boolean[n];
+        boolean[] flag = new boolean[n];
+
+        boolean ans = true;
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                flag[i] = true;
+                ans = ans & solve(graph, i, visited, flag); 
+            }
+        }
+        
+        return ans;
+    }
+}
