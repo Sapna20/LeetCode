096b8eec8e22033320835347ce7c12b8819849fc
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-07-25 12:20:25 +0530

    Time: 23 ms (21.24%), Space: 143.8 MB (18.29%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,50 +1,47 @@
 class Solution {
 
-    int[] DIR_X = {1, -1, 0, 0};
-    int[] DIR_Y = {0, 0, 1, -1};
+    private int[] dirA = {1, 0, -1, 0};
+    private int[] dirB = {0, 1, 0, -1};
 
-    private boolean isValid(int i, int j, int m, int n, int parent_i, int parent_j) {
-        return i >= 0 && j >= 0 && i < m && j < n && !(parent_i == i && parent_j == j);
+    private boolean isValid(int row, int col, int m, int n) {
+        return row < m && col < n && row >= 0 && col >=0;
     }
 
-    private boolean dfs(char[][] grid, boolean[][] isVisited, int i, int j, int parent_i, int parent_j, int m, int n) {
-
-        if(isVisited[i][j]) {
-            return true;
+    private boolean detectCycle(char[][] grid, boolean[][] visited, int row, int col, int m, int n, int pRow, int pCol) {
+        visited[row][col] = true;
+        boolean cycle = false;
+        for(int i=0; i<4; i++) {
+            int nRow = row + dirA[i];
+            int nCol = col + dirB[i];
+            if(isValid(nRow, nCol, m, n) && grid[row][col] == grid[nRow][nCol]) {
+                if(!visited[nRow][nCol]) {
+                    cycle = cycle || detectCycle(grid, visited, nRow, nCol, m, n, row, col);
+                    if(cycle) {
+                        return cycle;
+                    }
+                } else if (!(pRow == nRow && pCol == nCol)) {
+                    return true;
+                }
+            }
         }
-
-        isVisited[i][j] = true;
-        boolean ans = false;
-
-        for(int p=0; p<4; p++) {
-            int new_i = i + DIR_X[p];
-            int new_j = j + DIR_Y[p];
-            if(isValid(new_i, new_j, m, n, parent_i, parent_j) && grid[i][j] == grid[new_i][new_j]) {
-                ans = ans || dfs(grid, isVisited, new_i, new_j, i, j, m, n);
-            } 
-        }
-        return ans;
+        return cycle;
     }
 
     public boolean containsCycle(char[][] grid) {
         int m = grid.length;
-        if(m == 0) {
-            return false;
-        }
         int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
 
-        boolean[][] isVisited = new boolean[m][n];
-        boolean ans = false;
+        boolean cycle = false;
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(!isVisited[i][j]) {
-                    ans = ans || dfs(grid, isVisited, i, j, -1, -1, m, n);
+                if(!visited[i][j]) {
+                    cycle = cycle || detectCycle(grid, visited, i, j, m, n, -1, -1);
                 }
-                isVisited[i][j] = true; 
             }
         }
-        
-        return ans;
+
+        return cycle;
     }
 }
