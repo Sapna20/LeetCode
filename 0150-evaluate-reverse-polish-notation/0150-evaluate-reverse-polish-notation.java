89ce1c9e93d3850bfc30e4371bee3ea334bbf0f3
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-10 12:02:15 +0530

    Time: 6 ms (87.15%), Space: 45.3 MB (63.4%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,36 +1,35 @@
 class Solution {
 
-    private int resolve(String op, int a, int b) {
-        if(op.equals("+")) {
-            return b+a;
-        }
-        if(op.equals("-")) {
-            return b-a;
-        }
-        if(op.equals("*")) {
-            return b*a;
-        }
-        if(op.equals("/")) {
-            return b/a;
-        }
+    private boolean isOperator(String str) {
+        return str.equals("*") || str.equals("+") || str.equals("-") || str.equals("/");
+    }
 
-        return 1;
+    private int calculate(int num1, int num2, String str) {
+        if(str.equals("+")) {
+            return num1+num2;
+        } else if(str.equals("*")) {
+            return num1*num2;
+        } else if(str.equals("-")) {
+            return num1-num2;
+        } else {
+            return num1/num2;
+        }
     }
 
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st = new Stack<Integer>();
-        List<String> operator = new ArrayList<String>(Arrays.asList("+", "-", "*", "/"));
+        Stack<Integer> stack = new Stack<Integer>();
 
         for(String str : tokens) {
-            if(operator.contains(str)) {
-                int a = st.pop();
-                int b = st.pop();
-                st.push(resolve(str, a, b));
+            if(isOperator(str)) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                int ans = calculate(num1, num2, str);
+                stack.push(ans);
             } else {
-                st.push(Integer.valueOf(str));
+                stack.push(Integer.parseInt(str));
             }
         }
 
-        return st.pop();
+        return stack.peek();
     }
 }
