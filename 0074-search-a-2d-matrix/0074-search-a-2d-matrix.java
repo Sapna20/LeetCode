48083df6b1d2aa6bf6095000dc499fdb166ae690
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-11 15:33:41 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.37%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,39 +1,43 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int start = 0, end = m-1;
+    private int findRow(int[][] matrix, int target) {
+        int start = 0;
+        int end = matrix.length-1;
 
         while(start <= end) {
-            int mid = (end - start)/2 + start;
-            if(matrix[mid][0] == target)
-                return true;
-            else if (matrix[mid][0] > target) 
+            int mid = (end-start)/2 + start;
+            if(matrix[mid][0] == target) {
+                return mid;
+            } else if (matrix[mid][0] > target) {
                 end = mid-1;
-            else
+            } else {
                 start = mid+1;
-        }
-        System.out.println(end);
-
-        if(end >= 0) {
-            int row = end;
-            
-            start = 0;
-            end = n-1;
-
-            while(start <= end) {
-                int mid = (end - start)/2 + start;
-                if(matrix[row][mid] == target)
-                    return true;
-                else if (matrix[row][mid] > target) 
-                    end = mid-1;
-                else
-                    start = mid+1;
             }
         }
+        return end;
+    }
 
+    private boolean findElement(int[][] matrix, int target, int row) {
+        int start = 0;
+        int end = matrix[row].length-1;
+
+        while(start <= end) {
+            int mid = (end-start)/2 + start;
+            if(matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
         return false;
+    }
 
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = findRow(matrix, target);
+        if (row == -1) {
+            return false;
+        }
+        return findElement(matrix, target, row);
     }
 }
