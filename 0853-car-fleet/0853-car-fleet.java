a957d1e435664b1da1d558dce10e63caafea21c7
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-17 12:19:48 +0530

    Time: 72 ms (86.52%), Space: 98.1 MB (60.55%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,22 +1,39 @@
+class Car {
+    int position;
+    double time;
+
+    Car(int pos, double t) {
+        position = pos;
+        time = t;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public double getTime() {
+        return this.time;
+    }
+}
+
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int res = 0;
-        double[] timeArr = new double[target];
-        for (int i = 0; i < position.length; i++)
-        {
-            timeArr[position[i]]= (double)(target - position[i]) / speed[i];
+        int n = position.length;
+        Car[] cars = new Car[n];
+
+        for(int i=0; i<n; i++) {
+            double time = (double)(target - position[i])/(double)speed[i];
+            cars[i] = new Car(position[i], time);
         }
-        
-        double prev = 0.0;
-        for (int i = target-1; i >=0 ; i--)
-        {
-            double cur = timeArr[i];
-            if (cur > prev)
-            {
-                prev = cur;
-                res++;
+        Arrays.sort(cars, (a, b) -> (a.getPosition() - b.getPosition()));
+
+        Stack<Car> stack = new Stack<Car>();
+        for(int i=n-1; i>=0; i--) {
+            if(stack.isEmpty() || cars[i].getTime() > stack.peek().getTime()) {
+                stack.push(cars[i]);
             }
         }
-        return res;   
+
+        return stack.size();
     }
 }
