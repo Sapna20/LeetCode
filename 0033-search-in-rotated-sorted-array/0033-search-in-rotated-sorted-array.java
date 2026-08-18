e194a9e4ceca9d90b88c956af2cbb62794870240
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-18 11:31:12 +0530

    Time: 3 ms (1.39%), Space: 43.9 MB (28.4%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,48 +1,50 @@
 class Solution {
 
-    private int searchStartIndex(int[] nums) {
+    private int findPivotIndex(int[] nums) {
         int n = nums.length;
         int start = 0;
         int end = n-1;
 
         while(start <= end) {
             int mid = (end-start)/2 + start;
-            if(nums[mid] <= nums[(mid-1+n) % n]) {
+            if(nums[mid] <= nums[(mid+1)%n] && nums[mid] <= nums[(mid-1+n)%n]) {
                 return mid;
-            } else if (nums[mid] < nums[start]) {
-                end = mid-1;
-            } else if (nums[mid] > nums[end]) {
+            } else if(nums[mid] > nums[end]) {
                 start = mid+1;
             } else {
                 end = mid-1;
             }
         }
-
-        return Integer.MAX_VALUE;
+        return -1;
     }
 
-    private int binarySearch(int[] nums, int target, int start, int end) {
+    private int binarySearch(int[] nums, int start, int end, int target) {
         while(start <= end) {
             int mid = (end-start)/2 + start;
-            if(target == nums[mid]) {
+            if(nums[mid] == target) {
                 return mid;
-            } else if (target > nums[mid]) {
+            } else if (nums[mid] < target) {
                 start = mid+1;
             } else {
                 end = mid-1;
             }
         }
-
         return -1;
     }
 
     public int search(int[] nums, int target) {
-        int startIndex = searchStartIndex(nums);
+        int pivotIdx = findPivotIndex(nums);
+        System.out.println(pivotIdx);
+     
+        if(pivotIdx == -1) {
+            return -1;
+        } 
+
         int n = nums.length;
-        
-        return target >= nums[startIndex] && target <= nums[n-1] 
-                            ? binarySearch(nums, target, startIndex, nums.length-1)
-                            : binarySearch(nums, target, 0, startIndex-1);
-        
+
+        int firstSearch = binarySearch(nums, 0, (pivotIdx-1+n)%n, target);
+        int secondSearch = binarySearch(nums, pivotIdx, n-1, target);
+
+        return firstSearch != -1 ? firstSearch : secondSearch;
     }
 }
