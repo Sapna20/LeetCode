565b61f723d4d944eceed3dbe82dfdb49965dbfd
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-21 11:29:54 +0530

    Time: 73 ms (5.05%), Space: 47 MB (24.24%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,29 +1,31 @@
 class Solution {
+
+    private int findMaxFrequency(Map<Character, Integer> fmap) {
+        int fmax = 0;
+        for(int x : fmap.values()) {
+            fmax = Math.max(x, fmax);
+        }
+        return fmax;
+    }
+
     public int characterReplacement(String s, int k) {
+        Map<Character, Integer> fmap = new HashMap<>();
         int n = s.length();
-        int i=0, j=0, ans=0;
+        int i=0, j=0, ans = 0;
 
-        int[] freq = new int[26];
-
-        while(j<n) {
-            char chRight = s.charAt(j);
-            freq[chRight - 'A']++;
-            int maxf = 0;
-            for(int f : freq) {
-                maxf = Math.max(f, maxf);
-            }
-
-            if(j-i+1 - maxf <= k) {
-                ans = Math.max(ans, j-i+1);
-            } else {
-                char chLeft = s.charAt(i);
-                freq[chLeft - 'A']--;
+        while(j < n) {
+            char c = s.charAt(j);
+            fmap.put(c, fmap.getOrDefault(c, 0)+1);
+            
+            int fmax = findMaxFrequency(fmap);
+            while(k < j-i+1 - fmax) {
+                fmap.put(s.charAt(i), fmap.get(s.charAt(i)) - 1);
                 i++;
+                fmax = findMaxFrequency(fmap);
             }
-
+            ans = Math.max(ans, j-i+1);
             j++;
         }
-
         return ans;
     }
 }
