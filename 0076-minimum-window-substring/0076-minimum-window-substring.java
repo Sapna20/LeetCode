765fb71cc8a434b4384ab725f2d3845d8e150f46
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-24 17:11:11 +0530

    Time: 166 ms (5.78%), Space: 48.4 MB (5.12%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,52 +1,58 @@
 class Solution {
+
+    private boolean isSubMap(Map<Character, Integer> tmap, Map<Character, Integer> smap) {
+        for(Map.Entry<Character, Integer> entry : tmap.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            if(!smap.containsKey(key) || smap.get(key) < value) {
+                return false;
+            } 
+        }
+       
+        return true;
+    }
+
     public String minWindow(String s, String t) {
-        int n = s.length(), m = t.length();
-        Map<Character, Integer> map = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        int m = s.length();
+        int n = t.length();
         String ans = "";
 
-        for(int i=0; i<m; i++) {
-            char ch = t.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        if(n > m) {
+            return ans;
         }
 
         int i=0, j=0;
+        Map<Character, Integer> tmap = new HashMap<>();
+        Map<Character, Integer> smap = new HashMap<>();
 
-        while(j < n) {
-            char rightChar = s.charAt(j);
-            if(map.containsKey(rightChar)) {
-                map2.put(rightChar, map2.getOrDefault(rightChar, 0)+1);
+        for(i=0; i<t.length(); i++) {
+            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0)+1);
+        }
+
+        i=0;
+        while(j < m) { // ABC
+            if(!tmap.containsKey(s.charAt(j))) {
+                j++;
+                continue;
             }
 
-            // System.out.println("j included = " + s.substring(i, j+1));
-
-            while(isSubMap(map, map2)) {
-                if(ans.length() == 0 || j-i+1 < ans.length()) {
-                    ans = s.substring(i, j+1);
-                }
-                char leftChar = s.charAt(i);
-                if(map2.containsKey(leftChar)) {
-                    map2.put(leftChar, map2.get(leftChar)-1);
-                }
-                i++;
-                // System.out.println("i excluded = " + s.substring(i, j+1));
-
-            }
+            smap.put(s.charAt(j), smap.getOrDefault(s.charAt(j), 0)+1);
             j++;
-        }
-
-        return ans;
-    }
-
-    private boolean isSubMap(Map<Character, Integer> map, Map<Character, Integer> map2) {
-        for(Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if(map2.containsKey(entry.getKey()) 
-                && map2.get(entry.getKey()) >= entry.getValue()) {
-                    continue;
-            } else {
-                return false;
+            if(isSubMap(tmap, smap)) {
+                while(!tmap.containsKey(s.charAt(i)) || tmap.get(s.charAt(i)) < smap.get(s.charAt(i))) {
+                    
+                    if(!smap.containsKey(s.charAt(i)))  {
+                        i++;
+                        continue;
+                    }
+                    smap.put(s.charAt(i), smap.get(s.charAt(i))-1);
+                    i++;
+                }
+                if(ans.length() == 0 || ans.length() > s.substring(i, j).length()) {
+                    ans = s.substring(i, j);
+                }
             }
         }
-        return true;
+        return ans;
     }
 }
