82eac5a7933c0f8fd5178d0e6045fa74d0eaa87c
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-08-30 17:51:24 +0530

    Time: 16 ms (70.25%), Space: 47.2 MB (21.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,38 +1,29 @@
 class Solution {
-
-    private int solve(int[] coins, int amount, int idx, Integer[][] dp) {
-        if(amount <= 0) {
-            return 0;
-        }
-        if(idx < 0) {
-            return 10001;
-        }
-
-        if(dp[idx][amount] != null) {
-            return dp[idx][amount];
-        }
-
-        if(amount - coins[idx] >= 0) {
-            dp[idx][amount] = Math.min(
-                1 + solve(coins, amount - coins[idx], idx, dp),
-                solve(coins, amount, idx-1, dp)
-                );
-            return dp[idx][amount];
-        }
-
-        dp[idx][amount] = solve(coins, amount, idx-1, dp);
-        return dp[idx][amount];
-    }
-
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        Integer[][] dp = new Integer[n][amount+1];
+        int[][] dp = new int[n+1][amount+1];
 
-        int count = solve(coins, amount, coins.length-1, dp);
+        for(int i=0; i<=amount; i++) {
+            dp[0][i] = Integer.MAX_VALUE-1;
+        }
 
-        System.out.println(Integer.MAX_VALUE);
-        return count > 10000 ? -1 : count;
+        for(int i=0; i<=n; i++) {
+            dp[i][0] = 0;
+        }
 
-        // during revision write bottom up approach
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=amount; j++) {
+                if(j - coins[i-1] >= 0) {
+                    dp[i][j] = Math.min(
+                        1 + dp[i][j-coins[i-1]],
+                        dp[i-1][j]
+                    );
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][amount] == Integer.MAX_VALUE-1 ? -1 : dp[n][amount];
     }
 }
