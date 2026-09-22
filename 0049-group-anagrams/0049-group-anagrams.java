9d99e3d608149f8ba755131c337aed6813efb9d3
Sapna Sharma <40593744+Sapna20@users.noreply.github.com>
2026-09-22 16:06:05 +0530

    Time: 7 ms (72.18%), Space: 49.4 MB (84.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
 
-        // TIME - O(N) SPACE - O(N)
-
-        List<List<String>> ans = new ArrayList<List<String>>();
-        Map<String, List<String>> map = new HashMap<String, List<String>>();
-
-        for(String str : strs) {                      // O(N)
-            char[] carr = str.toCharArray();
-            Arrays.sort(carr);                      // O(100 log 100) -> O(1);
-            String key = new String(carr);
+        for(String str : strs) {
+            char[] strArr = str.toCharArray();
+            Arrays.sort(strArr);
+            String key = new String(strArr);
             if(map.containsKey(key)) {
-                map.get(key).add(str);             // O(1)
+                map.get(key).add(str);
             } else {
-                List<String> ls = new ArrayList<String>();
+                List<String> ls = new ArrayList<>();
                 ls.add(str);
-                map.put(key, ls);                   // O(1)
+                map.put(key, ls);
             }
         }
 
+        List<List<String>> ans = new ArrayList<List<String>>();
 
-        for(List<String> ls : map.values()) {    // O(N)
-            ans.add(ls);
+        for(List<String> ls : map.values()) {
+            ans.add(new ArrayList<String>(ls));
         }
 
         return ans;
